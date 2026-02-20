853a98979c5740ea98d044a2a90514a2fc0d3b08
Christopher Priebe <chrisp9090@gmail.com>
2026-02-19 20:48:22 -0800

    Updated webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,38 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
+    const isProduction = (argv.mode || "production") === "production";
 
-  return {
-    entry: "./_js/index.js",
-    output: {
-      path: path.resolve(__dirname, "assets/js"),
-      filename: "bundle.js",
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "../css/main.css", 
-      }),
-      new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery",
-        "window.jQuery": "jquery"
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader"
-          ],
+    return {
+        mode: argv.mode,
+        entry: "./_js/index.js",
+        output: {
+            path: path.resolve(__dirname, "assets/js"),
+            filename: "bundle.js",
         },
-      ],
-    },
-  };
+        optimization: {
+            minimize: isProduction,
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "../css/main.css", 
+            }),
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                "window.jQuery": "jquery"
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader"
+                    ],
+                },
+            ],
+        },
+    };
 };
